7ca7acb39d6b3b03e0a94a1cc95cdc6c13d97bb3
Igor Paes <igorvargasp@hotmail.com>
2025-10-23 05:49:43 -0300

    refactor: layout + navbar and about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from "react"
-import { Content } from "./components/Content/Content"
-import { Menu } from "./components/Menu/Menu"
-import { AboutPage } from "./pages/AboutPage"
-
-
-
-export const menuItems = [
-  { name: "About", component: () => <AboutPage /> },
-  { name: "Projects", href: "#" },
-  { name: "Skills", href: "#" },
-  { name: "Work Experiences", href: "#" },
-]
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 
 
 function App() {
-  const [currentMenu, setCurrentMenu] = React.useState("About");
+
 
   return (
-    <div className="flex flex-row h-screen w-screen">
-      <Menu setCurrentMenu={setCurrentMenu} menuItems={menuItems} />
-      <Content currentMenu={currentMenu} setCurrentMenu={setCurrentMenu} menuItems={menuItems} />
+    <div className="font-sora scroll-smooth overflow-x-hidden">
+      <Navbar />
+      <Home />
+
+
     </div>
   )
 }
